f2faa066a79a975090995e8820c662bd1aae3e70
Denis Andronov <denisunderonov@gmail.com>
2024-05-16 00:54:11 +0300

    finish getOne()

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,8 +1,8 @@
 import PostModel from "../models/Post.js";
 
-export const getAll = async (req,res) => {
+export const getAll = async (req, res) => {
   try {
-    const posts = await PostModel.find().populate('user');
+    const posts = await PostModel.find().populate("user");
     res.json(posts);
   } catch (error) {
     res.status(500).json({
@@ -11,20 +11,44 @@ export const getAll = async (req,res) => {
   }
 };
 
-export const getOne = async (req,res) => {
+export const getOne = async (req, res) => {
   try {
     const postId = req.params.id;
-    
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      },
+      (err, doc) => {
+        if (err) {
+          return res.status(500).json({
+            message: "Не удалось вернуть статью",
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+        res.json(doc);
+      }
+    );
   } catch (error) {
     res.status(500).json({
-      message: "Не удалось получить статьи",
+      message: "Не удалось получить статью",
     });
   }
 };
 
 export const create = async (req, res) => {
   try {
-    
     const doc = new PostModel({
       title: req.body.title,
       text: req.body.text,
@@ -32,7 +56,7 @@ export const create = async (req, res) => {
       imageUrl: req.body.imageUrl,
       user: req.userId,
     });
-    
+
     const post = await doc.save();
 
     res.json(post);
